6350477b3df692847aa2e20259acf95a65a57a86
surendhar <surendhark240@gmail.com>
2024-11-02 13:33:24 +0530

    Changes applied

--- src/Pages/RestaurantPage.jsx
+++ src/Pages/RestaurantPage.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 
 const restaurantData = {
-  1: { name: 'Restaurant A', availability: '10 meals', expiry: '2 hours' },
-  2: { name: 'Restaurant B', availability: '5 meals', expiry: '1 hour' },
+  1: { name: 'Udupi Hotel Akshaya', availability: '10 meals', expiry: '2 hours' },//
+  2: { name: 'Madurai Kumar Mess', availability: '5 meals', expiry: '1 hour' },//
+  3: { name: 'Geetham Veg Restaurant', availability: '5 meals', expiry: '1 hour' },//
+  4: { name: 'Coal Barbecues', availability: '5 meals', expiry: '1 hour' },
+  5: { name: 'Grill Box R K Salai', availability: '10 meals', expiry: '2 hours' },//
+  6: { name: 'DR Kitchen', availability: '5 meals', expiry: '1 hour' },//
+  7: { name: 'Dindigal Thalapakatti Biriyani', availability: '5 meals', expiry: '1 hour' },//
+  8: { name: 'Charminar Biriyani Centre', availability: '5 meals', expiry: '1 hour' },
 };
 
 const RestaurantPage = () => {
